de0332e2004c5ef5a3bc46232195567e55178c5c
Umair Ahmed <umair170@hotmail.com>
2018-09-24 18:11:41 +0100

    add fetch more story functionality

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BounceLoader from "react-spinners/BounceLoader";
+import Button from "@material-ui/core/Button";
 
 import BASE_URL from "../API_URL";
 import StoryBox from "./Components/StoryBox";
@@ -8,6 +9,7 @@ export default class Home extends Component {
   state = {
     stories: [],
     loading: true,
+    storiesCount: 0,
   };
 
   componentDidMount = () => {
@@ -15,17 +17,26 @@ export default class Home extends Component {
   };
 
   fetchStories = () => {
-    this.setState({ loading: true });
+    this.setState({
+      loading: true,
+      storiesCount: this.state.storiesCount + 10,
+    });
+
     fetch(`${BASE_URL}/topstories.json`)
       .then(res => res.json())
       .then(stories => {
-        const top10 = stories.slice(0, 10);
+        const sliceStories =
+          this.state.storiesCount === 10 ? 0 : this.state.storiesCount;
+        const top10 = stories.slice(sliceStories, sliceStories + 10);
         top10.map(eachID =>
           fetch(`${BASE_URL}/item/${eachID}.json`)
             .then(res => res.json())
             .then(story => {
               let topStories = this.state.stories.concat(story);
-              this.setState({ stories: topStories, loading: false });
+              this.setState({
+                stories: topStories,
+                loading: false,
+              });
             })
             .catch(err => console.warn(err))
         );
@@ -36,38 +47,46 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        {this.state.loading ? (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <BounceLoader
-              sizeUnit={"px"}
-              size={150}
-              color={"#123abc"}
-              loading={this.state.loading}
+        <div>
+          {this.state.stories.map((story, index) => (
+            <StoryBox
+              key={story.id}
+              id={story.id}
+              title={story.title}
+              url={story.url}
+              comments={story.descendants}
+              score={story.score}
+              index={index}
+              time={story.time}
+              author={story.by}
             />
-          </div>
-        ) : (
-          <div>
-            {this.state.stories.map((story, index) => (
-              <StoryBox
-                key={story.id}
-                id={story.id}
-                title={story.title}
-                url={story.url}
-                comments={story.descendants}
-                score={story.score}
-                index={index}
-                time={story.time}
-                author={story.by}
+          ))}
+
+          {this.state.loading ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <BounceLoader
+                sizeUnit={"px"}
+                size={150}
+                color={"#123abc"}
+                loading={this.state.loading}
               />
-            ))}
-            <p>More Stories ...</p>
-          </div>
-        )}
+            </div>
+          ) : (
+            <Button
+              variant="outlined"
+              color="primary"
+              // className={classes.button}
+              onClick={() => this.fetchStories()}
+            >
+              More Stories ...
+            </Button>
+          )}
+        </div>
       </div>
     );
   }
