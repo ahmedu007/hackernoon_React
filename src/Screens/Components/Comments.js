458f1083c8be9e3786b3c50ac77a7623cb2871a3
Umair Ahmed <umair170@hotmail.com>
2018-09-24 17:36:52 +0100

    replace List with card on the UI

--- src/Screens/Components/Comments.js
+++ src/Screens/Components/Comments.js
@@ -1,16 +1,52 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const Comments = props => {
   const date = new Date(props.time * 1000);
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
   return (
     <Fragment>
-      <List>
+      <Card className={classes.card}>
+        {props.deleted ? (
+          <CardContent>
+            <Typography variant="headline" component="h1">
+              This comment was deleted
+            </Typography>
+          </CardContent>
+        ) : (
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary">
+              <span>{`Comment by: ${props.author.charAt(0).toUpperCase() +
+                props.author.slice(1)} `}</span>
+            </Typography>
+            <Typography variant="headline" component="h1">
+              <div
+                dangerouslySetInnerHTML={{ __html: props.comment }}
+                style={{ fontSize: "1rem" }}
+              />
+            </Typography>
+            <Typography
+              className={classes.pos}
+              color="textSecondary"
+              component="p"
+            >
+              {date.toDateString()}
+            </Typography>
+            <Typography>
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+        )}
+      </Card>
+      {/* <List>
         <ListItem>
           {props.deleted ? (
             <ListItemText primary="This comment was deleted" />
@@ -30,7 +66,7 @@ const Comments = props => {
             />
           )}
         </ListItem>
-      </List>
+      </List> */}
 
       <Divider />
     </Fragment>
